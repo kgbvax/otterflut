0797f8ecbbdbe508ce69edbb8d2ecb249d8e9728
kgbavx <vax@kgbvax.net>
2018-04-10 20:17:22 +0200

    baby server

---
+++ counter.go
@@ -0,0 +1,211 @@
+package main
+
+// lifted from https://github.com/alexsasharegan/go-simple-tcp-server/blob/master/counter.go
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+// Counter is a container for tracking and managing the runtime counters.
+type Counter struct {
+	mu sync.RWMutex
+	// Uniq is a map of the unique numbers received during uptime.
+	Uniq map[int]bool
+	// Cnt valid numbers received during uptime.
+	Cnt int
+	// IntvlCnt is the total valid numbers received during output interval.
+	IntvlCnt int
+	Log      *struct {
+		// Cnt is the log rotation counter.
+		Cnt int
+		// w is a buffered writer to the current log entry
+		w *bufio.Writer
+		f io.Closer
+	}
+	intvl *struct {
+		output  chan bool
+		logging chan bool
+	}
+	// Sem is a semaphore to do request limiting.
+	Sem chan int
+}
+
+var logFmt = "logs/data.%d.log"
+
+// NewCounter constructs a new Counter.
+func NewCounter(connLimit int) *Counter {
+	f := openLogFile(fmt.Sprintf(logFmt, 0))
+	return &Counter{
+		Uniq: make(map[int]bool),
+		Sem:  make(chan int, connLimit),
+		Log: &struct {
+			Cnt int
+			w   *bufio.Writer
+			f   io.Closer
+		}{
+			w: bufio.NewWriter(f),
+			f: f,
+		},
+		intvl: &struct {
+			output  chan bool
+			logging chan bool
+		}{
+			output:  make(chan bool),
+			logging: make(chan bool),
+		},
+	}
+}
+
+func openLogFile(name string) *os.File {
+	f, err := os.OpenFile(
+		name,
+		// We only need to write to the log,
+		// but we need to create if file does not exist,
+		// or else truncate it if we're re-opening it on a new run.
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+		0666)
+
+	if err != nil {
+		log.Fatalf("could not open log file: %v", err)
+	}
+
+	return f
+}
+
+// FlushClose writes the log contents to disk and closes the file.
+func (c *Counter) FlushClose() (err error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	err = c.Log.w.Flush()
+	if err != nil {
+		return fmt.Errorf("could not flush log to disk: %v", err)
+	}
+
+	err = c.Log.f.Close()
+	if err != nil {
+		return fmt.Errorf("could not close log file: %v", err)
+	}
+
+	return
+}
+
+// FlushRotate writes the log contents to disk, closes, and rotates the log file.
+func (c *Counter) FlushRotate() (err error) {
+	err = c.FlushClose()
+	if err != nil {
+		return
+	}
+
+	c.mu.Lock()
+	c.Log.Cnt++
+	f := openLogFile(fmt.Sprintf(logFmt, c.Log.Cnt))
+
+	c.Log.f = f
+	c.Log.w = bufio.NewWriter(f)
+	c.mu.Unlock()
+
+	return
+}
+
+// RecordUniq adds a unique int to the map and the log buffer in a thread safe way.
+func (c *Counter) RecordUniq(num int) (err error) {
+	c.mu.Lock()
+	c.Uniq[num] = true
+	_, err = c.Log.w.WriteString(fmt.Sprintf("%d\n", num))
+	c.mu.Unlock()
+	return err
+}
+
+func (c *Counter) outputCounters() {
+	// We could use a read lock first,
+	// then grab a write lock to clear counter.
+	c.mu.Lock()
+
+	fmt.Printf(
+		"----------------\n"+
+			"Count unique: %d\n"+
+			"Count total : %d\n"+
+			"Count last  : %d\n",
+		len(c.Uniq),
+		c.Cnt,
+		c.IntvlCnt)
+	c.IntvlCnt = 0
+
+	c.mu.Unlock()
+}
+
+// RunOutputInterval outputs the counters on an interval.
+// It takes a nil channel that the caller will close to stop execution.
+// Must be run on go routine.
+func (c *Counter) RunOutputInterval(intvl time.Duration) {
+	for {
+		select {
+		case <-time.After(intvl):
+			c.outputCounters()
+		case <-c.intvl.output:
+			return
+		}
+	}
+}
+
+// StopOutputIntvl exits the output interval by closing it's underlying nil channel.
+func (c *Counter) StopOutputIntvl() {
+	close(c.intvl.output)
+}
+
+// RunLogInterval outputs the counters on an interval.
+// It takes a nil channel that the caller will close to stop execution.
+// Must be run on go routine.
+func (c *Counter) RunLogInterval(intvl time.Duration) {
+	var err error
+	for {
+		select {
+		case <-time.After(intvl):
+			err = c.FlushRotate()
+			if err != nil {
+				log.Fatalf("could not flush and rotate logs: %v", err)
+			}
+		case <-c.intvl.logging:
+			err = c.FlushClose()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error flushing log to disk: %v", err)
+			}
+			return
+		}
+	}
+}
+
+// StopLogIntvl exits the output interval by closing it's underlying nil channel.
+func (c *Counter) StopLogIntvl() {
+	close(c.intvl.logging)
+}
+
+// Inc increments the counters in a thread safe way.
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.Cnt++
+	c.IntvlCnt++
+	c.mu.Unlock()
+}
+
+// HasValue checks if an int has been recorded in a thread safe way.
+func (c *Counter) HasValue(num int) (b bool) {
+	c.mu.RLock()
+	b = c.Uniq[num]
+	c.mu.RUnlock()
+	return
+}
+
+// Close closes all internals and flushes logs to disk.
+func (c *Counter) Close() (err error) {
+	c.StopOutputIntvl()
+	c.StopLogIntvl()
+	return
+}
--- server.go
+++ server.go
@@ -4,8 +4,89 @@ import (
 	"os"
 	"net"
 	"log"
+	"os/signal"
+	"syscall"
+
+	"fmt"
+	"bufio"
+
 )
 
+var port string = "1234"
+var connLimit int = 1024
+
+// Or as a kind user on reddit refactored:
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal("ERROR:", err)
+	}
+}
+
+
+
+
+// acceptConns uses the semaphore channel on the counter to rate limit.
+// New connections get sent on the returned channel.
+func acceptConns(srv net.Listener, counter *Counter) <-chan net.Conn {
+	conns := make(chan net.Conn)
+
+	go func() {
+		for {
+			conn, err := srv.Accept()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error accepting connection: %v\n", err)
+				continue
+			}
+
+			select {
+			case counter.Sem <- 1:
+				conns <- conn
+			default:
+				fmt.Fprintf(conn, "Server busy.")
+				conn.Close()
+			}
+		}
+	}()
+
+	return conns
+}
+
+
+// Handles incoming requests.
+// Handles closing of the connection.
+func handleConnection(conn net.Conn, counter *Counter) {
+	// Defer all close logic.
+	// Using a closure makes it easy to group logic as well as execute serially
+	// and avoid the deferred LIFO exec order.
+	defer func() {
+		// Since handleConnection is run in a go routine,
+		// it manages the closing of our net.Conn.
+		conn.Close()
+		// Once our connection is closed,
+		// we can drain a value from our semaphore
+		// to free up a space in the connection limit.
+		<-counter.Sem
+	}()
+
+	scanner := bufio.NewScanner(conn)
+	var s string
+	for scanner.Scan() {
+		s = scanner.Text()
+
+
+		/* From here on out, we have a valid input. */
+		// Safely increment total counter.
+		counter.Inc()
+
+	}
+
+	// If a failure to read input occurs,
+	// it's probably my bad.
+	// Fail and figure it out if so!
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading: %v", err)
+	}
+}
 
 func main() {
 	hostname,_ := os.Hostname()
@@ -13,5 +94,35 @@ func main() {
 	addrs,_:= net.LookupHost(hostname)
 	log.Printf("my ips: %v",addrs)
 
+	addr:=addrs[0] //TODO
 
+	srv, err := net.Listen("tcp", addr+":"+port)
+	checkErr(err)
+	defer srv.Close()
+
+	counter := NewCounter(connLimit)
+
+	// Listen for termination signals.
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+
+
+
+	// Set up intervals
+	go counter.RunOutputInterval(5)
+	go counter.RunLogInterval(5)
+
+	// Receive new connections on an unbuffered channel.
+	conns := acceptConns(srv, counter)
+	for {
+		select {
+		case conn := <-conns:
+			go handleConnection(conn, counter)
+		case <-sig:
+			// Add a leading new line since the signal escape sequence prints on stdout.
+			fmt.Printf("\nShutting down server.\n")
+			counter.Close()
+			os.Exit(0)
+		}
+	}
 }
