0448d53e5a2438a8bcf4c1f7895aae7761a5189d
kgbavx <vax@kgbvax.net>
2018-05-03 22:18:09 +0200

    gles2 namespace

--- ofgles.go
+++ ofgles.go
@@ -39,7 +39,7 @@ func initGl() {
 	}
 	window.MakeContextCurrent()
 
-	if err := gl.Init(); err != nil {
+	if err := gles2.Init(); err != nil {
 		panic(err)
 	}
 
@@ -59,40 +59,40 @@ func ofGlPollEvents() {
 
 func makeTexture() uint32 {
 	var texture uint32
-	gl.Enable(gl.TEXTURE_2D)
-	gl.GenTextures(1, &texture)
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexImage2D(
-		gl.TEXTURE_2D,
+	gles2.Enable(gles2.TEXTURE_2D)
+	gles2.GenTextures(1, &texture)
+	gles2.BindTexture(gles2.TEXTURE_2D, texture)
+	gles2.TexParameteri(gles2.TEXTURE_2D, gles2.TEXTURE_MIN_FILTER, gles2.LINEAR)
+	gles2.TexParameteri(gles2.TEXTURE_2D, gles2.TEXTURE_MAG_FILTER, gles2.LINEAR)
+	gles2.TexParameteri(gles2.TEXTURE_2D, gles2.TEXTURE_WRAP_S, gles2.CLAMP_TO_EDGE)
+	gles2.TexParameteri(gles2.TEXTURE_2D, gles2.TEXTURE_WRAP_T, gles2.CLAMP_TO_EDGE)
+	gles2.TexImage2D(
+		gles2.TEXTURE_2D,
 		0,
-		gl.RGBA,
+		gles2.RGBA,
 		int32(W),
 		int32(H),
 		0,
-		gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		gl.Ptr(*pixels))
+		gles2.RGBA,
+		gles2.UNSIGNED_BYTE,
+		gles2.Ptr(*pixels))
 
 
 	return texture
 }
 
 func setupScene() {
-	//gl.Enable(gl.DEPTH_TEST)
+	//gles2.Enable(gles2.DEPTH_TEST)
 
-	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
-	gl.ClearDepth(1)
-	gl.DepthFunc(gl.LEQUAL)
+	gles2.ClearColor(0.0, 0.0, 0.0, 0.0)
+	gles2.ClearDepth(1)
+	gles2.DepthFunc(gles2.LEQUAL)
 
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadIdentity()
-	gl.Frustum(-1, 1, -1, 1, 1.0, 10.0)
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
+	gles2.MatrixMode(gles2.PROJECTION)
+	gles2.LoadIdentity()
+	gles2.Frustum(-1, 1, -1, 1, 1.0, 10.0)
+	gles2.MatrixMode(gles2.MODELVIEW)
+	gles2.LoadIdentity()
 }
 
 func destroyScene() {
@@ -104,27 +104,27 @@ func tearDown() {
 }
 
 func drawScene() {
-	gl.Clear(gl.COLOR_BUFFER_BIT)
+	gles2.Clear(gles2.COLOR_BUFFER_BIT)
 
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-	gl.Translatef(0, 0, -0.0000001) //I have no idea what I am doing ;-)
-	gl.Rotatef(0, 0, 0, 0)
+	gles2.MatrixMode(gles2.MODELVIEW)
+	gles2.LoadIdentity()
+	gles2.Translatef(0, 0, -0.0000001) //I have no idea what I am doing ;-)
+	gles2.Rotatef(0, 0, 0, 0)
 
 	var texture = makeTexture()
 
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.Color4f(1, 1, 1, 1)
+	gles2.BindTexture(gles2.TEXTURE_2D, texture)
+	gles2.Color4f(1, 1, 1, 1)
 
-	gl.Begin(gl.QUADS)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, -1, -1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, -1)
-	gl.End()
+	gles2.Begin(gles2.QUADS)
+	gles2.TexCoord2f(0, 0)
+	gles2.Vertex3f(-1, -1, -1)
+	gles2.TexCoord2f(1, 0)
+	gles2.Vertex3f(1, -1, -1)
+	gles2.TexCoord2f(1, 1)
+	gles2.Vertex3f(1, 1, -1)
+	gles2.TexCoord2f(0, 1)
+	gles2.Vertex3f(-1, 1, -1)
+	gles2.End()
 }
 
